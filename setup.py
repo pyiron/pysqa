bc2f9ae15267d41181f37f8e7dd8f72e2ff4a2dc
Jan Janßen <janssen@mpie.de>
2019-02-11 11:02:36 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='pysqa',
+    version='0.0.1',
+    description='pysqa - simple queue adapter',
+    long_description='https://github.com/pysqa/pysqa',
+
+    url='https://github.com/pysqa/pysqa',
+    author='Jan Janssen',
+    author_email='janssen@mpie.de',
+    license='BSD',
+
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+
+    keywords='pysqa',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=['defusedxml',
+                      'importlib',
+                      'jinja2',
+                      'pandas',
+                      'pyyaml'],
+    )
