9088f3b74d747972ca4fd4cded72a3121c0af4ea
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-07-15 10:09:13 +0200

    Add pandas import

--- pysqa/wrapper/slurm.py
+++ pysqa/wrapper/slurm.py
@@ -2,6 +2,9 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+import pandas
+
+
 __author__ = "Jan Janssen"
 __copyright__ = "Copyright 2019, Max-Planck-Institut für Eisenforschung GmbH - " \
                 "Computational Materials Design (CM) Department"
