fe70885e3706f959d7e9ca6a280ba73773bbbcc0
Jan Janßen <janssen@mpie.de>
2019-08-18 17:22:14 +0200

    copy Sanders changes

--- pysqa/queueadapter.py
+++ pysqa/queueadapter.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # Copyright (c) Jan Janssen
+# Updated for HPC UGent by Sander Borgmans
 
 import getpass
 import importlib
@@ -7,7 +8,7 @@ from jinja2 import Template
 import os
 import pandas
 import subprocess
-from yaml import load
+import yaml
 
 __author__ = "Jan Janssen"
 __copyright__ = "Copyright 2019, Jan Janssen"
@@ -19,7 +20,33 @@ __date__ = "Feb 9, 2019"
 
 
 class QueueAdapter(object):
-    def __init__(self, directory='.'):
+    """
+    The goal of the QueueAdapter class is to make submitting to a queue system as easy as starting another sub process
+    locally.
+
+    Args:
+        directory (str): directory containing the queue.yaml files as well as corresponding jinja2 templates for the
+                         individual queues.
+
+    Attributes:
+
+        .. attribute:: config
+
+            QueueAdapter configuration read from the queue.yaml file.
+
+        .. attribute:: queue_list
+
+            List of available queues
+
+        .. attribute:: queue_view
+
+            Pandas DataFrame representation of the available queues, read from queue.yaml.
+
+        .. attribute:: queues
+
+            Queues available for auto completion QueueAdapter().queues.<queue name> returns the queue name.
+    """
+    def __init__(self, directory='~/.queues'):
         self._config = self._read_config(file_name=os.path.join(directory, 'queue.yaml'))
         self._fill_queue_dict(queue_lst_dict=self._config['queues'])
         self._load_templates(queue_lst_dict=self._config['queues'], directory=directory)
@@ -41,6 +68,7 @@ class QueueAdapter(object):
         else:
             raise ValueError()
         self._commands = getattr(importlib.import_module(module_name), class_name)()
+        self._queues = Queues(self.queue_list)
 
     @property
     def config(self):
@@ -69,6 +97,10 @@ class QueueAdapter(object):
         """
         return pandas.DataFrame(self._config['queues']).T.drop(['script', 'template'], axis=1)
 
+    @property
+    def queues(self):
+        return self._queues
+
     def submit_job(self, queue=None, job_name=None, working_directory=None, cores=None, memory_max=None,
                    run_time_max=None, command=None):
         """
@@ -87,6 +119,7 @@ class QueueAdapter(object):
         """
         if isinstance(command, list):
             command = ''.join(command)
+
         queue_script = self._job_submission_template(queue=queue, job_name=job_name,
                                                      working_directory=working_directory, cores=cores,
                                                      memory_max=memory_max, run_time_max=run_time_max, command=command)
@@ -94,10 +127,13 @@ class QueueAdapter(object):
         with open(queue_script_path, 'w') as f:
             f.writelines(queue_script)
         out = self._execute_command(commands_lst=self._commands.submit_job_command + [queue_script_path],
-                                    working_directory=working_directory, split_output=False)
-        return int(out)
+                                    working_directory=working_directory, split_output=False, queue=queue) # adapted
+        if out is not None:
+            return self._commands.get_job_id_from_output(out)
+        else:
+            return None
 
-    def enable_reservation(self, process_id):
+    def enable_reservation(self, process_id, reservation_flag, queue=None): # not implemented I guess
         """
 
         Args:
@@ -106,10 +142,14 @@ class QueueAdapter(object):
         Returns:
             str:
         """
-        return self._execute_command(commands_lst=self._commands.enable_reservation_command + [str(process_id)],
-                                     split_output=True)[0]
+        out = self._execute_command(commands_lst=self._commands.enable_reservation_command + ['x=FLAGS:ADVRES:'+reservation_flag] + [str(process_id)],
+                                    split_output=True, queue=queue)
+        if out is not None:
+            return out[0]
+        else:
+            return None
 
-    def delete_job(self, process_id):
+    def delete_job(self, process_id, queue=None):
         """
 
         Args:
@@ -118,10 +158,14 @@ class QueueAdapter(object):
         Returns:
             str:
         """
-        return self._execute_command(commands_lst=self._commands.delete_job_command + [str(process_id)],
-                                     split_output=True)[0]
+        out = self._execute_command(commands_lst=self._commands.delete_job_command + [str(process_id)],
+                                    split_output=True, queue=queue)
+        if out is not None:
+            return out[0]
+        else:
+            return None
 
-    def get_queue_status(self, user=None):
+    def get_queue_status(self, user=None, queue=None):
         """
 
         Args:
@@ -130,9 +174,9 @@ class QueueAdapter(object):
         Returns:
             pandas.DataFrame:
         """
-        out = self._execute_command(commands_lst=self._commands.get_queue_status_command, split_output=False)
+        out = self._execute_command(commands_lst=self._commands.get_queue_status_command, split_output=False, queue=queue)
         df = self._commands.convert_queue_status(queue_status_output=out)
-        if user is None:
+        if user is None or df.empty:
             return df
         else:
             return df[df['user'] == user]
@@ -143,9 +187,14 @@ class QueueAdapter(object):
         Returns:
            pandas.DataFrame:
         """
-        return self.get_queue_status(user=self._get_user())
+        df = pandas.DataFrame()
+        
+        for queue in self.queue_list:
+            user = self._get_user()
+            df = df.append(self.get_queue_status(user=user, queue=queue), ignore_index=True, sort=False)
+        return df
 
-    def get_status_of_job(self, process_id):
+    def get_status_of_job(self, process_id, queue=None):
         """
 
         Args:
@@ -154,14 +203,14 @@ class QueueAdapter(object):
         Returns:
              str: ['running', 'pending', 'error']
         """
-        df = self.get_queue_status()
+        df = self.get_queue_status(queue=queue)
         df_selected = df[df['jobid'] == process_id]['status']
         if len(df_selected) != 0:
             return df_selected.values[0]
         else:
             return None
 
-    def check_queue_parameters(self, queue, cores=1, run_time_max=None, memory_max=None, active_queue=None):
+    def check_queue_parameters(self, queue, cores=None, run_time_max=None, memory_max=None, active_queue=None): #adapted
         """
 
         Args:
@@ -179,13 +228,17 @@ class QueueAdapter(object):
         cores = self._value_in_range(value=cores,
                                      value_min=active_queue['cores_min'],
                                      value_max=active_queue['cores_max'])
+
+        nodes = (cores-1)//(active_queue['cores_per_node']) + 1
+
         run_time_max = self._value_in_range(value=run_time_max,
                                             value_max=active_queue['run_time_max'])
-        memory_max = self._value_in_range(value=memory_max,
-                                          value_max=active_queue['memory_max'])
+
+        memory_max = self._value_in_range(value=memory_max, # assume in bytes!
+                                          value_max=active_queue['mem_per_node_max']*nodes*1024**3)
         return cores, run_time_max, memory_max
 
-    def _job_submission_template(self, queue=None, job_name='job.py', working_directory='.', cores=None,
+    def _job_submission_template(self, queue=None, job_name=None, working_directory='.', cores=None,
                                  memory_max=None, run_time_max=None, command=None):
         """
 
@@ -207,15 +260,21 @@ class QueueAdapter(object):
         if queue not in self.queue_list:
             raise ValueError()
         active_queue = self._config['queues'][queue]
-        cores, run_time_max, memory_max = self.check_queue_parameters(queue=None,
+        cores, run_time_max, memory_max = self.check_queue_parameters(queue=queue,
                                                                       cores=cores,
                                                                       run_time_max=run_time_max,
                                                                       memory_max=memory_max,
                                                                       active_queue=active_queue)
+        nodes = (cores-1)//(active_queue['cores_per_node']) + 1
+        cores = int((cores-1)%active_queue['cores_per_node']) + 1
+        memory_max = self._get_size(memory_max) + 'B'
+        run_time_max = "{}:{}:{}".format(int(run_time_max//3600),int((run_time_max%3600)//60),int(run_time_max%60))
+
         template = active_queue['template']
         return template.render(job_name=job_name,
                                working_directory=working_directory,
                                cores=cores,
+                               nodes=nodes,
                                memory_max=memory_max,
                                run_time_max=run_time_max,
                                command=command)
@@ -230,7 +289,7 @@ class QueueAdapter(object):
         return getpass.getuser()
 
     @staticmethod
-    def _execute_command(commands_lst, working_directory=None, split_output=True):
+    def _execute_command(commands_lst, working_directory=None, split_output=True, queue=None):
         """
 
         Args:
@@ -241,17 +300,24 @@ class QueueAdapter(object):
         Returns:
             str:
         """
+        
+        cmd = ""
+        if not queue is None:
+            cmd += "module --quiet swap cluster/{}; ".format(queue)
+        cmd += " ".join(commands_lst)
+       
         if working_directory is None:
             try:
-                out = subprocess.check_output(commands_lst, stderr=subprocess.STDOUT, universal_newlines=True)
+                out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
             except subprocess.CalledProcessError:
                 out = None
         else:
             try:
-                out = subprocess.check_output(commands_lst, cwd=working_directory, stderr=subprocess.STDOUT,
-                                              universal_newlines=True)
+                out = subprocess.check_output(cmd, cwd=working_directory, stderr=subprocess.STDOUT, shell=True)
             except subprocess.CalledProcessError:
                 out = None
+                
+        out = out.decode('utf8')
         if out is not None and split_output:
             return out.split('\n')
         else:
@@ -268,7 +334,7 @@ class QueueAdapter(object):
             dict:
         """
         with open(file_name, 'r') as f:
-            return load(f)
+            return yaml.load(f, Loader=yaml.FullLoader)
 
     @staticmethod
     def _fill_queue_dict(queue_lst_dict):
@@ -330,3 +396,37 @@ class QueueAdapter(object):
             if value_max is not None:
                 return value_max
             return value
+
+    @staticmethod
+    def _get_size(size):
+        system = [
+        (1024 ** 5, 'P'),
+        (1024 ** 4, 'T'),
+        (1024 ** 3, 'G'),
+        (1024 ** 2, 'M'),
+        (1024 ** 1, 'K'),
+        (1024 ** 0, 'B'),
+        ]
+        for factor, suffix in system:
+            if size >= factor:
+                break
+        amount = int(size/factor)
+        return str(amount) + suffix
+
+
+class Queues(object):
+    """
+    Queues is an abstract class simply to make the list of queues available for auto completion. This is mainly used in
+    interactive environments like jupyter.
+    """
+    def __init__(self, list_of_queues):
+        self._list_of_queues = list_of_queues
+
+    def __getattr__(self, item):
+        if item in self._list_of_queues:
+            return item
+        else:
+            raise AttributeError
+
+    def __dir__(self):
+        return self._list_of_queues
--- pysqa/wrapper/slurm.py
+++ pysqa/wrapper/slurm.py
@@ -2,6 +2,9 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+import pandas
+
+
 __author__ = "Jan Janssen"
 __copyright__ = "Copyright 2019, Max-Planck-Institut für Eisenforschung GmbH - " \
                 "Computational Materials Design (CM) Department"
@@ -22,13 +25,33 @@ class SlurmCommands(object):
         return ['scancel']
 
     @property
-    def enable_reservation_command(self):
-        raise NotImplementedError()
+    def enable_reservation_command(self): # this is written in TORQUE (compatible with hpc)
+        return ['qalter', '-W']
 
     @property
     def get_queue_status_command(self):
-        return ['squeue']
+        return ['squeue', '--format', '"%A|%u|%t|%j"', '--noheader']
+
+    @staticmethod
+    def get_job_id_from_output(queue_submit_output):
+          return int(queue_submit_output.splitlines()[-1].rstrip().lstrip().split(';')[0])  # adapted here (gives string jobid;cluster instead of jobid)
+
+    @staticmethod
+    def get_queue_from_output(queue_submit_output):
+          return str(queue_submit_output.splitlines()[-1].rstrip().lstrip().split(';')[1])
 
     @staticmethod
     def convert_queue_status(queue_status_output):
-        raise NotImplementedError()
+        qstat = queue_status_output.splitlines()
+        queue = qstat[0].split(':')[1].strip() # get queue name
+        if len(qstat) <= 1: # first row contains cluster name, check if there are jobs
+            return pandas.DataFrame(columns=['cluster', 'jobid', 'user', 'status', 'jobname'])
+       
+        line_split_lst = [line.split('|') for line in qstat[1:]] # adapted here (starts from 1 since first line gives cluster)
+        job_id_lst, user_lst, status_lst, job_name_lst, queue_lst = zip(*[(int(jobid), user, status.lower(), jobname, queue)
+                                                               for jobid, user, status, jobname in line_split_lst])
+        return pandas.DataFrame({'cluster': queue_lst,
+                                 'jobid': job_id_lst,
+                                 'user': user_lst,
+                                 'jobname': job_name_lst,
+                                 'status': status_lst})
