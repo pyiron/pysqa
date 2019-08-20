279da989b9bb042001543e1d184fb50ba0f3fc06
Jan Janßen <janssen@mpie.de>
2019-08-20 17:22:20 +0200

    update tests

--- tests/test_queueadapter.py
+++ tests/test_queueadapter.py
@@ -6,6 +6,7 @@ import pandas
 import unittest
 import getpass
 from pysqa import QueueAdapter
+from pysqa.basic import BasisQueueAdapter
 
 __author__ = "Jan Janssen"
 __copyright__ = "Copyright 2019, Jan Janssen"
@@ -42,42 +43,42 @@ class TestRunmode(unittest.TestCase):
         self.assertEqual(self.moab.config['queue_primary'], 'moab')
 
     def test_value_in_range(self):
-        self.assertEqual(None, self.sge._value_in_range(value=None, value_min=None, value_max=None))
-        self.assertEqual(1, self.sge._value_in_range(value=None, value_min=1, value_max=None))
-        self.assertEqual(1, self.sge._value_in_range(value=None, value_min=None, value_max=1))
-        self.assertEqual(1, self.sge._value_in_range(value=1, value_min=None, value_max=None))
-        self.assertEqual(1, self.sge._value_in_range(value=0, value_min=1, value_max=None))
-        self.assertEqual(1, self.sge._value_in_range(value=2, value_min=None, value_max=1))
-        self.assertEqual(1, self.sge._value_in_range(value=1, value_min=0, value_max=2))
+        self.assertEqual(None, self.sge._adapter._value_in_range(value=None, value_min=None, value_max=None))
+        self.assertEqual(1, self.sge._adapter._value_in_range(value=None, value_min=1, value_max=None))
+        self.assertEqual(1, self.sge._adapter._value_in_range(value=None, value_min=None, value_max=1))
+        self.assertEqual(1, self.sge._adapter._value_in_range(value=1, value_min=None, value_max=None))
+        self.assertEqual(1, self.sge._adapter._value_in_range(value=0, value_min=1, value_max=None))
+        self.assertEqual(1, self.sge._adapter._value_in_range(value=2, value_min=None, value_max=1))
+        self.assertEqual(1, self.sge._adapter._value_in_range(value=1, value_min=0, value_max=2))
 
     def test_job_submission_template(self):
-        self.assertRaises(ValueError, self.sge._job_submission_template, command=None)
-        self.assertRaises(TypeError, self.sge._job_submission_template, command=1)
+        self.assertRaises(ValueError, self.sge._adapter._job_submission_template, command=None)
+        self.assertRaises(TypeError, self.sge._adapter._job_submission_template, command=1)
         template = "#!/bin/bash\n#$ -N job.py\n#$ -wd .\n#$ -pe impi_hydra_small 1\n#$ -l h_rt=604800\n" \
                    "#$ -o time.out\n#$ -e error.out\n\npython test.py"
-        self.assertEqual(self.sge._job_submission_template(command='python test.py'), template)
+        self.assertEqual(self.sge._adapter._job_submission_template(command='python test.py'), template)
         template = "#!/bin/bash\n#BSUB -q queue\n#BSUB -J job.py\n#BSUB -o time.out\n#BSUB -n 10\n#BSUB -cwd .\n" \
                    "#BSUB -e error.out\n#BSUB -W 259200\n\npython test.py"
-        self.assertEqual(self.lsf._job_submission_template(command='python test.py'), template)
-        self.assertRaises(ValueError, self.sge._job_submission_template, command='python test.py', queue='notavailable')
+        self.assertEqual(self.lsf._adapter._job_submission_template(command='python test.py'), template)
+        self.assertRaises(ValueError, self.sge._adapter._job_submission_template, command='python test.py', queue='notavailable')
 
     def test_interfaces(self):
-        self.assertEqual(self.sge._commands.submit_job_command, ['qsub', '-terse'])
-        self.assertEqual(self.sge._commands.delete_job_command, ['qdel'])
-        self.assertEqual(self.sge._commands.enable_reservation_command, ['qalter', '-R', 'y'])
-        self.assertEqual(self.sge._commands.get_queue_status_command, ['qstat', '-xml'])
-        self.assertEqual(self.torque._commands.submit_job_command, ['qsub', '-terse'])
-        self.assertEqual(self.torque._commands.delete_job_command, ['qdel'])
-        self.assertEqual(self.torque._commands.get_queue_status_command, ['qstat', '-x'])
-        self.assertEqual(self.lsf._commands.submit_job_command, ['bsub', '-terse'])
-        self.assertEqual(self.lsf._commands.delete_job_command, ['bkill'])
-        self.assertEqual(self.lsf._commands.get_queue_status_command, ['qstat', '-x'])
-        self.assertEqual(self.slurm._commands.submit_job_command, ['sbatch', '--parsable'])
-        self.assertEqual(self.slurm._commands.delete_job_command, ['scancel'])
-        self.assertEqual(self.slurm._commands.get_queue_status_command, ['squeue', '--format', '%A|%u|%t|%j', '--noheader'])
-        self.assertEqual(self.moab._commands.submit_job_command, ['msub'])
-        self.assertEqual(self.moab._commands.delete_job_command, ['mjobctl', '-c'])
-        self.assertEqual(self.moab._commands.get_queue_status_command, ['mdiag', '-x'])
+        self.assertEqual(self.sge._adapter._commands.submit_job_command, ['qsub', '-terse'])
+        self.assertEqual(self.sge._adapter._commands.delete_job_command, ['qdel'])
+        self.assertEqual(self.sge._adapter._commands.enable_reservation_command, ['qalter', '-R', 'y'])
+        self.assertEqual(self.sge._adapter._commands.get_queue_status_command, ['qstat', '-xml'])
+        self.assertEqual(self.torque._adapter._commands.submit_job_command, ['qsub', '-terse'])
+        self.assertEqual(self.torque._adapter._commands.delete_job_command, ['qdel'])
+        self.assertEqual(self.torque._adapter._commands.get_queue_status_command, ['qstat', '-x'])
+        self.assertEqual(self.lsf._adapter._commands.submit_job_command, ['bsub', '-terse'])
+        self.assertEqual(self.lsf._adapter._commands.delete_job_command, ['bkill'])
+        self.assertEqual(self.lsf._adapter._commands.get_queue_status_command, ['qstat', '-x'])
+        self.assertEqual(self.slurm._adapter._commands.submit_job_command, ['sbatch', '--parsable'])
+        self.assertEqual(self.slurm._adapter._commands.delete_job_command, ['scancel'])
+        self.assertEqual(self.slurm._adapter._commands.get_queue_status_command, ['squeue', '--format', '%A|%u|%t|%j', '--noheader'])
+        self.assertEqual(self.moab._adapter._commands.submit_job_command, ['msub'])
+        self.assertEqual(self.moab._adapter._commands.delete_job_command, ['mjobctl', '-c'])
+        self.assertEqual(self.moab._adapter._commands.get_queue_status_command, ['mdiag', '-x'])
 
     def test_convert_queue_status(self):
         with open(os.path.join(self.path, 'config/sge', 'qstat.xml'), 'r') as f:
@@ -89,7 +90,7 @@ class TestRunmode(unittest.TestCase):
         df_merge = df_running.append(df_pending, sort=True)
         df = pandas.DataFrame({'jobid': pandas.to_numeric(df_merge.jobid), 'user': df_merge.user,
                                'jobname': df_merge.jobname, 'status': df_merge.status})
-        self.assertTrue(df.equals(self.sge._commands.convert_queue_status(queue_status_output=content)))
+        self.assertTrue(df.equals(self.sge._adapter._commands.convert_queue_status(queue_status_output=content)))
 
     def test_queue_list(self):
         self.assertEqual(sorted(self.sge.queue_list), ['impi_hy', 'impi_hydra', 'impi_hydra_cmfe', 'impi_hydra_small'])
@@ -101,7 +102,7 @@ class TestRunmode(unittest.TestCase):
             _ = self.sge.queues.notavailable
 
     def test_get_user(self):
-        self.assertEqual(self.sge._get_user(), getpass.getuser())
+        self.assertEqual(self.sge._adapter._get_user(), getpass.getuser())
 
     def test_check_queue_parameters(self):
         self.assertEqual((1, 604800, None), self.sge.check_queue_parameters(queue='impi_hydra_small'))
@@ -110,14 +111,14 @@ class TestRunmode(unittest.TestCase):
         self.assertIsInstance(self.slurm.queue_view, pandas.DataFrame)
 
     def test_memory_string_comparison(self):
-        self.assertEqual(QueueAdapter._value_in_range(1023, value_min='1K'), '1K')
-        self.assertEqual(QueueAdapter._value_in_range(1035, value_min='1K'), 1035)
-        self.assertEqual(QueueAdapter._value_in_range(1035, value_max='1K'), '1K')
-        self.assertEqual(QueueAdapter._value_in_range('1035', value_min='1K'), '1035')
-        self.assertEqual(QueueAdapter._value_in_range('60000M', value_min='1K', value_max='50G'), '50G')
-        self.assertEqual(QueueAdapter._value_in_range('60000', value_min='1K', value_max='50G'), '50G')
-        self.assertEqual(QueueAdapter._value_in_range('60000M', value_min='1K', value_max='70G'), '60000M')
-        self.assertEqual(QueueAdapter._value_in_range(60000, value_min='1K', value_max='70G'), 60000)
-        self.assertEqual(QueueAdapter._value_in_range(90000 * 1024 ** 2, value_min='1K', value_max='70G'), '70G')
-        self.assertEqual(QueueAdapter._value_in_range('90000', value_min='1K', value_max='70G'), '70G')
-        self.assertEqual(QueueAdapter._value_in_range('60000M', value_min='60G', value_max='70G'), '60G')
+        self.assertEqual(BasisQueueAdapter._value_in_range(1023, value_min='1K'), '1K')
+        self.assertEqual(BasisQueueAdapter._value_in_range(1035, value_min='1K'), 1035)
+        self.assertEqual(BasisQueueAdapter._value_in_range(1035, value_max='1K'), '1K')
+        self.assertEqual(BasisQueueAdapter._value_in_range('1035', value_min='1K'), '1035')
+        self.assertEqual(BasisQueueAdapter._value_in_range('60000M', value_min='1K', value_max='50G'), '50G')
+        self.assertEqual(BasisQueueAdapter._value_in_range('60000', value_min='1K', value_max='50G'), '50G')
+        self.assertEqual(BasisQueueAdapter._value_in_range('60000M', value_min='1K', value_max='70G'), '60000M')
+        self.assertEqual(BasisQueueAdapter._value_in_range(60000, value_min='1K', value_max='70G'), 60000)
+        self.assertEqual(BasisQueueAdapter._value_in_range(90000 * 1024 ** 2, value_min='1K', value_max='70G'), '70G')
+        self.assertEqual(BasisQueueAdapter._value_in_range('90000', value_min='1K', value_max='70G'), '70G')
+        self.assertEqual(BasisQueueAdapter._value_in_range('60000M', value_min='60G', value_max='70G'), '60G')
