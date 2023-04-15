7f157cd4f20daed53a24e6b007436af6523568cd
Han <h.mai@mpie.de>
2023-04-15 15:55:16 +1000

    Update instructs for jinja adjustment for torque

--- pysqa/wrapper/torque.py
+++ pysqa/wrapper/torque.py
@@ -31,7 +31,8 @@ class TorqueCommands(SchedulerCommands):
     
     @staticmethod
     def get_job_id_from_output(queue_submit_output):
-        # strip last line from output, leading and trailing whitespaces, and separates the queue id from the trailing ".suffix"
+        # strip last line from output, leading and trailing whitespaces, and separates the queue id from the stuff after "." 
+        # Adjust if your system doesn't have output like below!
         # e.g. qsub run_queue.sh -> "12347673.gadi-pbs", the below returns 12347673
         # It must return an integer for it to not raise an exception later.
         return int(queue_submit_output.splitlines()[-1].rstrip().lstrip().split(sep=".")[0])    
