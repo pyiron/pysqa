9c54451bd18d6c7ac2e738273df408eeabf2754b
Han <h.mai@mpie.de>
2023-04-18 08:25:16 +1000

    Implement method convert_queue_status for torque

--- pysqa/wrapper/torque.py
+++ pysqa/wrapper/torque.py
@@ -3,6 +3,8 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 from pysqa.wrapper.generic import SchedulerCommands
+import subprocess
+import pandas as pd
 
 __author__ = "Jan Janssen"
 __copyright__ = (
@@ -27,7 +29,7 @@ class TorqueCommands(SchedulerCommands):
 
     @property
     def get_queue_status_command(self):
-        return ["qstat", "-x"]
+        return ["qstat", "-f"]
 
     @staticmethod
     def get_job_id_from_output(queue_submit_output):
@@ -38,3 +40,65 @@ class TorqueCommands(SchedulerCommands):
         return int(
             queue_submit_output.splitlines()[-1].rstrip().lstrip().split(sep=".")[0]
         )
+
+    @staticmethod
+    def convert_queue_status(queue_status_output):
+
+        # # Run the qstat -f command and capture its output
+        # output = subprocess.check_output(["qstat", "-f"])
+        
+        # Split the output into lines
+        lines = queue_status_output.decode().split("\n")
+
+        # Extract the job ID, user, job name, status, and working directory for each running job
+        job_id = None
+        user = None
+        job_name = None
+        status = None
+        working_dir = None
+        job_id_lst = []
+        user_lst = []
+        job_name_lst = []
+        status_lst = []
+        working_directory_lst = []
+
+        for line in lines:
+            if line.startswith("Job Id:"):
+                # Start a new job entry
+                job_id = line.split()[-1].split(sep=".")[0]
+            elif line.startswith("    Job_Name ="):
+                job_name = line.split("=")[-1].strip()
+            elif line.startswith("    Job_Owner ="):
+                user = line.split("=")[-1].split("@")[0]
+            elif line.startswith("    job_state ="):
+                status = line.split("=")[-1].strip()
+            elif "PBS_O_WORKDIR" in line:
+                working_dir = line.split()[-1].split(sep="WORKDIR=")[-1]
+            elif line.strip() == "":
+                # End of job entry - add to lists 
+                # This if is necessary to avoid the final row containing None values...
+                if all(var is not None for var in (job_id, user, job_name, status, working_dir)):
+                    job_id_lst.append(job_id)
+                    user_lst.append(user)
+                    job_name_lst.append(job_name)
+                    status_lst.append(status)
+                    working_directory_lst.append(working_dir)
+                # Reset variables for next job
+                job_id = None
+                user = None
+                job_name = None
+                status = None
+                working_dir = None
+        # Create a DataFrame from the lists
+        df = pd.DataFrame(
+            {
+                "jobid": job_id_lst,
+                "user": user_lst,
+                "jobname": job_name_lst,
+                "status": status_lst,
+                "working_directory": working_directory_lst,
+            }
+        )
+        df["status"] = df["status"].apply(lambda x: "running" if x == "R" else "pending")
+
+        return df
