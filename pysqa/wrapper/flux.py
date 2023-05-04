3a72c898d607ac34b7341756fc278e5eee326925
Jan Janssen <janssen@lanl.gov>
2023-05-04 11:09:06 -0600

    Extend flux parser

--- pysqa/wrapper/flux.py
+++ pysqa/wrapper/flux.py
@@ -1,6 +1,7 @@
 # coding: utf-8
+import flux
+import pandas
 from pysqa.wrapper.generic import SchedulerCommands
-import subprocess
 
 
 class FluxCommands(SchedulerCommands):
@@ -18,8 +19,25 @@ class FluxCommands(SchedulerCommands):
 
     @staticmethod
     def get_job_id_from_output(queue_submit_output):
-        result = subprocess.run(
-            ['flux', 'job', 'id', queue_submit_output.splitlines()[-1].rstrip().lstrip().split()[-1]],
-            stdout=subprocess.PIPE
+        return flux.job.JobID(queue_submit_output.splitlines()[-1].rstrip().lstrip().split()[-1])
+
+    @staticmethod
+    def convert_queue_status(queue_status_output):
+        line_split_lst = [line.split() for line in queue_status_output.splitlines()]
+        job_id_lst, user_lst, job_name_lst, status_lst = [], [], [], []
+        for flux_id, user, job_name, status, task, nodes, runtime, ranks in line_split_lst:
+            job_id_lst.append(flux.job.JobID(flux_id))
+            user_lst.append(user)
+            job_name_lst.append(job_name)
+            status_lst.append(status)
+        df = pandas.DataFrame(
+            {
+                "jobid": job_id_lst,
+                "user": user_lst,
+                "jobname": job_name_lst,
+                "status": status_lst,
+            }
         )
-        return int(result.stdout.strip())
+        df.loc[df.status == "R", "status"] = "running"
+        df.loc[df.status == "S", "status"] = "pending"
+        return df
