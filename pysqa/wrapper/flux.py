2671ce06d38d5c20b939a108d3f0dba959aec5d4
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-05-11 19:58:28 -0600

    Fix for flux
    The entry can vary between 7 and 8 entries.

--- pysqa/wrapper/flux.py
+++ pysqa/wrapper/flux.py
@@ -25,20 +25,11 @@ class FluxCommands(SchedulerCommands):
     def convert_queue_status(queue_status_output):
         line_split_lst = [line.split() for line in queue_status_output.splitlines()]
         job_id_lst, user_lst, job_name_lst, status_lst = [], [], [], []
-        for (
-            flux_id,
-            user,
-            job_name,
-            status,
-            task,
-            nodes,
-            runtime,
-            ranks,
-        ) in line_split_lst:
-            job_id_lst.append(JobID(flux_id))
-            user_lst.append(user)
-            job_name_lst.append(job_name)
-            status_lst.append(status)
+        for entry in line_split_lst:
+            job_id_lst.append(JobID(entry[0]))
+            user_lst.append(entry[1])
+            job_name_lst.append(entry[2])
+            status_lst.append(entry[3])
         df = pandas.DataFrame(
             {
                 "jobid": job_id_lst,
