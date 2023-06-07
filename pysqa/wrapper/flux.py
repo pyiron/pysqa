41163b74a7ac619ed7d6bfc926ec4b2ad2e63641
Jan Janssen <janssen@lanl.gov>
2023-06-07 11:39:25 -0600

    Set status to finished

--- pysqa/wrapper/flux.py
+++ pysqa/wrapper/flux.py
@@ -40,4 +40,5 @@ class FluxCommands(SchedulerCommands):
         )
         df.loc[df.status == "R", "status"] = "running"
         df.loc[df.status == "S", "status"] = "pending"
+        df.loc[df.status == "CD", "status"] = "finished"
         return df
