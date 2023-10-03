7f012f93f009d3bf11d7cf4a92bef656e306be77
Liam Huber <liam.huber@gmail.com>
2023-10-03 11:34:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     extras_require={
         "sge": ['defusedxml==0.7.1'],
         "remote": ['paramiko==3.3.1', 'tqdm==4.66.1'],
-        "executor": ['pympipool==0.7.0', 'cloudpickle==2.2.1'],
+        "executor": ['pympipool==0.7.1', 'cloudpickle==2.2.1'],
     },
     cmdclass=versioneer.get_cmdclass(),
     entry_points={
