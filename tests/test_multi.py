36a311b995fdf5b380604b612866616482ae24fd
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-08-02 09:25:40 +0200

    Extend tests (#434)
    * Add test for empty config folder
    
    * add more tests
    
    * fix minimal

--- tests/test_multi.py
+++ tests/test_multi.py
@@ -29,3 +29,53 @@ class TestMultiQueueAdapter(unittest.TestCase):
 
     def test_list_clusters(self):
         self.assertEqual(self.multi.list_clusters(), ["local_slurm", "remote_slurm"])
+
+
+@unittest.skipIf(
+    skip_multi_test,
+    "Either paramiko or tqdm are not installed, so the multi queue adapter tests are skipped.",
+)
+class TestNoneAdapter(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.path = os.path.dirname(os.path.abspath(__file__))
+        cls.multi = QueueAdapter(
+            directory=os.path.join(cls.path, "config/multicluster")
+        )
+        cls.multi._adapter = None
+
+    def test_config(self):
+        self.assertIsNone(self.multi.config)
+
+    def test_queue_list(self):
+        self.assertIsNone(self.multi.queue_list)
+
+    def test_queue_view(self):
+        self.assertIsNone(self.multi.queue_view)
+
+    def test_queues(self):
+        self.assertIsNone(self.multi.queues)
+
+    def test_get_job_from_remote(self):
+        with self.assertRaises(TypeError):
+            self.multi.get_job_from_remote()
+
+    def test_transfer_file_to_remote(self):
+        with self.assertRaises(TypeError):
+            self.multi.transfer_file_to_remote()
+
+    def test_convert_path_to_remote(self):
+        with self.assertRaises(TypeError):
+            self.multi.convert_path_to_remote()
+
+    def test_check_queue_parameters(self):
+        cores_input = 1
+        run_time_input = 4320 * 60
+        cores, run_time_max, memory_max = self.multi.check_queue_parameters(
+            queue="test_queue",
+            cores=cores_input,
+            run_time_max=run_time_input,
+        )
+        self.assertEqual(cores_input, cores)
+        self.assertEqual(run_time_max, run_time_input)
+        self.assertIsNone(memory_max)
