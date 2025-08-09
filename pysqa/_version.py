f24aa5155bf2e3c4a3bbcedd61419eafa5a15d55
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-08-09 07:31:10 +0200

    Update _version.py

--- pysqa/_version.py
+++ pysqa/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev1+g0a7b083"
-__version_tuple__ = version_tuple = (0, 1, "dev1", "g0a7b083")
+__version__ = version = "0.0.1"
+__version_tuple__ = version_tuple = (0, 0, 1)
