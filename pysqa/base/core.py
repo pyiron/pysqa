a90fd475ec191c2325a46258099782b3ed2bd998
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-08-24 20:16:51 +0200

    Fix working directory creation (#454)

--- pysqa/base/core.py
+++ pysqa/base/core.py
@@ -394,8 +394,7 @@ class QueueAdapterCore(QueueAdapterAbstractClass):
             command=command,
             **kwargs,
         )
-        if not os.path.exists(working_directory):
-            os.makedirs(working_directory)
+        os.makedirs(working_directory, exist_ok=True)
         queue_script_path = os.path.join(working_directory, "run_queue.sh")
         with open(queue_script_path, "w") as f:
             f.writelines(queue_script)
