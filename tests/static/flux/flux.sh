d89df6c370dea76f1ee3ef45c27a8f3332333f03
Jan Janßen <janssen@mpie.de>
2026-02-21 12:24:28 +0100

    [Fix] The default runtime in flux is defined in seconds

--- tests/static/flux/flux.sh
+++ tests/static/flux/flux.sh
@@ -5,7 +5,7 @@
 # flux: --error=error.out
 # flux: -n {{cores}}
 {%- if run_time_max %}
-# flux: -t {{ [1, run_time_max // 60]|max }}
+# flux: -t {{run_time_max}}
 {%- endif %}
 {%- if dependency_list %}
 {%- for jobid in dependency_list %}
