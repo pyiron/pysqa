6345890cf0b25abb52fc23414c2357533d3a951e
Jan Janßen <janssen@mpie.de>
2026-03-27 22:00:18 +0100

    merge tests

--- tests/unit/base/test_config.py
+++ tests/unit/base/test_config.py
@@ -1,17 +1,24 @@
 import unittest
-from pysqa.base.config import QueueAdapterWithConfig
+import sys
+from unittest.mock import patch
 
 
 class TestConfig(unittest.TestCase):
     def test_bad_queue_type(self):
+        from pysqa.base.config import QueueAdapterWithConfig
+
         with self.assertRaises(ValueError):
             QueueAdapterWithConfig(config={"queue_type": "error", "queues": {}})
 
     def test_pydantic_validation_missing_queues(self):
+        from pysqa.base.config import QueueAdapterWithConfig
+
         with self.assertRaises(ValueError):
             QueueAdapterWithConfig(config={"queue_type": "SLURM"})
 
     def test_pydantic_validation_wrong_type(self):
+        from pysqa.base.config import QueueAdapterWithConfig
+
         with self.assertRaises(ValueError):
             QueueAdapterWithConfig(
                 config={
@@ -21,6 +28,8 @@ class TestConfig(unittest.TestCase):
             )
 
     def test_pydantic_validation_extra_fields(self):
+        from pysqa.base.config import QueueAdapterWithConfig
+
         config = {
             "queue_type": "SLURM",
             "queue_primary": "sq",
@@ -30,3 +39,23 @@ class TestConfig(unittest.TestCase):
         qa = QueueAdapterWithConfig(config=config)
         self.assertEqual(qa.config["queues"]["sq"]["extra_field"], "allowed")
         self.assertEqual(qa.config["extra_top_level"], "also_allowed")
+
+    def test_no_pydantic_validation_extra_fields(self):
+        with patch.dict('sys.modules', {'pydantic': None}):
+            if 'pysqa.base.models' in sys.modules:
+                del sys.modules['pysqa.base.models']
+
+            if 'pysqa.base.config' in sys.modules:
+                del sys.modules['pysqa.base.config']
+
+            from pysqa.base.config import QueueAdapterWithConfig
+
+            config = {
+                "queue_type": "SLURM",
+                "queue_primary": "sq",
+                "queues": {"sq": {"extra_field": "allowed"}},
+                "extra_top_level": "also_allowed",
+            }
+            qa = QueueAdapterWithConfig(config=config)
+            self.assertEqual(qa.config["queues"]["sq"]["extra_field"], "allowed")
+            self.assertEqual(qa.config["extra_top_level"], "also_allowed")
