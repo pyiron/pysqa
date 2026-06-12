758a90f3f0a78118dc0f6c2ffb6abe3db2b0cd4b
pyiron-runner <pyiron@mpie.de>
2026-06-12 08:12:20 +0000

    Format black

--- src/pysqa/_version.py
+++ src/pysqa/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev2+gbc2d23cd0'
-__version_tuple__ = version_tuple = (0, 1, 'dev2', 'gbc2d23cd0')
+__version__ = version = "0.1.dev2+gbc2d23cd0"
+__version_tuple__ = version_tuple = (0, 1, "dev2", "gbc2d23cd0")
 
 __commit_id__ = commit_id = None
